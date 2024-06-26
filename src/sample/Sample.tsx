a2faf2d01225783567aad585db5ba12101fe5beb
yo1030 <0vant1c@gmail.com>
2024-06-26 15:12:00 +0900

    Todo に immer 追加

--- src/sample/Sample.tsx
+++ src/sample/Sample.tsx
@@ -14,6 +14,7 @@ import { CanvasImmer } from "./CanvasImmer";
 import { ShoppingCartAdd } from "./ShoppingCartAdd";
 import { ShoppingCartDelete } from "./ShoppingCartDelete";
 import { TaskApp } from "./TaskApp";
+import { TaskAppImmer } from "./TaskAppImmer";
 
 export const Sample = () => {
   let i = 0;
@@ -51,6 +52,7 @@ export const Sample = () => {
         <ShoppingCartAdd></ShoppingCartAdd>
         <ShoppingCartDelete></ShoppingCartDelete>
         <TaskApp></TaskApp>
+        <TaskAppImmer></TaskAppImmer>
       </Masonry>
     </>
   )
